a892a0da408db7933f7ff4953991092362dbd62d
mhdkolo63 <151042988+mhdkolo63@users.noreply.github.com>
2026-09-11 06:20:43 +0100

    Update index.ts
    Fix

--- services/index.ts
+++ services/index.ts
@@ -1,4 +1,3 @@
-export { apiRequest, ApiError } from './api';
 export { authService } from './authService';
 export { lessonService } from './lessonService';
 export { progressService } from './progressService';
